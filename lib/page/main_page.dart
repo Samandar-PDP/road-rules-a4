ea4969f6956d8303475bde492c4efebf778436b4
samandar-me <samandarcoderdev@gmail.com>
2024-01-25 14:04:59 +0500

    commit

--- lib/page/main_page.dart
+++ lib/page/main_page.dart
@@ -12,37 +12,79 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   int _index = 0;
-  final _pages = [
-    HomePage()
-  ];
+  final _pages = [HomePage()];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: HomePage(),
+    final size = MediaQuery.of(context).size;
+
+    if (size.height > 500) {
+      return Scaffold(
+        body: Row(
+          children: [
+            NavigationRail(
+              trailing: FloatingActionButton(
+                backgroundColor: Colors.indigo,
+                shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(50))),
+                onPressed: () {},
+                child: const Icon(
+                  CupertinoIcons.add,
+                  color: Colors.white,
+                ),
+              ),
+              onDestinationSelected: (index) {
+                setState(() {
+                  _index = index;
+                });
+              },
+              destinations: const [
+                NavigationRailDestination(
+                    icon: Icon(Icons.home), label: Text("")),
+                NavigationRailDestination(
+                    icon: Icon(Icons.favorite), label: Text("")),
+                NavigationRailDestination(
+                    icon: Icon(Icons.info), label: Text("")),
+              ],
+              selectedIndex: _index,
+            ),
+            const VerticalDivider(),
+            Expanded(child: HomePage())
+          ],
+        ),
+      );
+    } else {
+      return Scaffold(
+        body: HomePage(),
         floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.indigo,
-          shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(50))),
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(50))),
           onPressed: () {},
-          child: const Icon(CupertinoIcons.add,color: Colors.white,),
+          child: const Icon(
+            CupertinoIcons.add,
+            color: Colors.white,
+          ),
         ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: AnimatedBottomNavigationBar(
-        inactiveColor: Colors.grey,
-        gapLocation: GapLocation.center,
-        onTap: (i) {
-          setState(() {
-            _index = i;
-          });
-        },
-        notchSmoothness: NotchSmoothness.smoothEdge,
-        activeIndex: _index,
-        icons: const [
-          CupertinoIcons.home,
-          CupertinoIcons.heart,
-          CupertinoIcons.info,
-          CupertinoIcons.profile_circled,
-        ],
-      ),
-    );
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        bottomNavigationBar: AnimatedBottomNavigationBar(
+          inactiveColor: Colors.grey,
+          gapLocation: GapLocation.center,
+          onTap: (i) {
+            setState(() {
+              _index = i;
+            });
+          },
+          notchSmoothness: NotchSmoothness.smoothEdge,
+          activeIndex: _index,
+          icons: const [
+            CupertinoIcons.home,
+            CupertinoIcons.heart,
+            CupertinoIcons.info,
+            CupertinoIcons.profile_circled,
+          ],
+        ),
+      );
+    }
   }
 }
